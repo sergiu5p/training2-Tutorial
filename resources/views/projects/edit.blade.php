33cf7f8d7bd7c1ffd943f8b739542fc97e15e199
sergiu5p <sergiu.purcariu@5psolutions.eu>
2019-08-20 12:43:20 +0300

    Add DELETE button

--- resources/views/projects/edit.blade.php
+++ resources/views/projects/edit.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <h1 class="title">Edit Project</h1>
 
-    <form method="POST" action="/projects/{{ $project->id }}">
+    <form method="POST" action="/projects/{{ $project->id }}" style="margin-bottom: 1em">
         {{ method_field('PATCH') }}
         {{ csrf_field() }}
 
@@ -26,6 +26,17 @@
         <div class="field">
             <div class="control">
                 <button type="submit" class="button is-link">Update Project</button>
+            </div>
+        </div>
+    </form>
+
+    <form method="POST" action="/projects/{{ $project->id }}">
+        {{ method_field('DELETE') }}
+        {{ csrf_field() }}
+
+        <div class="field">
+            <div class="control">
+                <button type="submit" class="button">Delete Project</button>
             </div>
         </div>
     </form>
