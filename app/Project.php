43d6423f4e4a4cb09c0e9742ae08a77775a2d0ee
sergiu5p <sergiu.purcariu@5psolutions.eu>
2019-08-20 17:10:39 +0300

    Create function tasks() who return the associated project

--- app/Project.php
+++ app/Project.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Project extends Model
 {
     protected $fillable = ['title', 'description'];
+
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
 }
