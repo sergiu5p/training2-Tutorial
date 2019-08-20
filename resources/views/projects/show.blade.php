d7c3193c803b33831799de24265e5c923c3bbc01
sergiu5p <sergiu.purcariu@5psolutions.eu>
2019-08-20 14:01:07 +0300

    Add edit button

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -4,4 +4,8 @@
     <h1 class="title">{{ $project->title }}</h1>
 
     <div class="content">{{ $project->description }}</div>
+
+    <p>
+        <a href="/projects/{{ $project->id }}/edit">Edit</a>
+    </p>
 @endsection
