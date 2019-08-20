47ff7189767d6b37ba734940d56d1f387b5a8cdc
sergiu5p <sergiu.purcariu@5psolutions.eu>
2019-08-20 11:49:53 +0300

    Add form and fields

--- resources/views/projects/edit.blade.php
+++ resources/views/projects/edit.blade.php
@@ -1,5 +1,29 @@
 @extends('layout')
 
 @section('content')
+    <h1 class="title">Edit Project</h1>
 
+    <form action="">
+        <div class="field">
+            <label for="title" class="label">Title</label>
+
+            <div class="control">
+                <input type="text" class="input" name="title" placeholder="Title">
+            </div>
+        </div>
+
+        <div class="field">
+            <label for="description" class="label">Description</label>
+
+            <div class="control">
+                <textarea name="description" class="textarea"></textarea>
+            </div>
+        </div>
+
+        <div class="field">
+            <div class="control">
+                <button type="submit" class="button is-link">Update Project</button>
+            </div>
+        </div>
+    </form>
 @endsection()
