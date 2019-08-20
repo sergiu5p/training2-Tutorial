4b0d53f02c34dbbb83a263f354d04bd4035202b4
sergiu5p <sergiu.purcariu@5psolutions.eu>
2019-08-20 11:32:08 +0300

    Modify the layout

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -2,18 +2,14 @@
 <html>
 
 <head>
-    <title>@yield('title', 'Laracasts')</title>
+    <title></title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.2/css/bulma.css">
 </head>
 
 <body>
-
-    <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="/about">About Us</a></li>
-        <li><a href="/contact">Contact</a></li>
-    </ul>
-
-    @yield('content')
+    <div class="container">
+        @yield('content')
+    </div>
 </body>
 
 </html>
