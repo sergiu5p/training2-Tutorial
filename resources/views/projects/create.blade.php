9191a14cc472a4894a52dd1a56d22173c616536a
sergiu5p <sergiu.purcariu@5psolutions.eu>
2019-08-20 15:52:53 +0300

    Rewrite all the file

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -1,25 +1,41 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title></title>
-</head>
-<body>
-    <h1>Projects</h1>
+@extends('layout')
 
-    <form method="POST" action="/projects">
-        {{ csrf_field() }}
+@section('content')
+    <h1 class="title">Edit Project</h1>
 
-        <div>
-            <input type="text" name="title" placeholder="Project title">
+    <form method="POST" action="/projects" style="margin-bottom: 1em">
+        @csrf
+
+        <div class="field">
+            <label for="title" class="label">Project Title</label>
+
+            <div class="control">
+                <input type="text" class="input {{ $errors->has('title') ? 'is-danger' : '' }}" name="title" placeholder="Title">
+            </div>
         </div>
 
-        <div>
-            <textarea name="description" placeholder="Project description"></textarea>
+        <div class="field">
+            <label for="description" class="label">Project Description</label>
+
+            <div class="control">
+                <textarea name="description" class="textarea {{ $errors->has('description') ? 'is-danger' : '' }}"></textarea>
+            </div>
         </div>
 
-        <div>
-            <button type="submit">Create Project</button>
+        <div class="field">
+            <div class="control">
+                <button type="submit" class="button is-link">Update Project</button>
+            </div>
         </div>
+
+        @if ($errors->any())
+            <div class="notification is-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
     </form>
-</body>
-</html>
+@endsection()
