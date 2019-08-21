ac1c72190dc7cc9700a3794ac1d56af6df9cfd78
sergiu5p <sergiu.purcariu@5psolutions.eu>
2019-08-21 11:37:03 +0300

    Add the form for the new task

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -12,14 +12,14 @@
     </div>
 
     @if ($project->tasks->count())
-        <div>
+        <div class="box">
             @foreach ($project->tasks as $task)
                 <div>
                     <form method="POST" action="/tasks/{{ $task->id }}">
                         @method('PATCH')
                         @csrf
 
-                        <label for="completed" class="checkbox">
+                        <label for="completed" class="checkbox {{ $task->completed ? 'is-complete' : '' }}">
                             <input
                                 type="checkbox"
                                 name="completed"
@@ -32,4 +32,22 @@
             @endforeach
         </div>
     @endif
+
+    <form method="POST" action="/projects/{{ $project->id }}/tasks" class="box">
+        @csrf
+
+        <div class="field">
+            <label for="Description" class="label">New Task</label>
+
+            <div class="control">
+                <input type="text" class="input" name="description" placeholder="New Task">
+            </div>
+        </div>
+
+        <div class="field">
+            <div class="control">
+                <button type="submit" class="button is-link">Add Task</button>
+            </div>
+        </div>
+    </form>
 @endsection
