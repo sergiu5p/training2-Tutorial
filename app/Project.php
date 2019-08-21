d310eb63019b0d3a7087bc7bfe5b7a2fecca617e
sergiu5p <sergiu.purcariu@5psolutions.eu>
2019-08-21 11:58:28 +0300

    In method store, optimize the code

--- app/Project.php
+++ app/Project.php
@@ -12,4 +12,9 @@ class Project extends Model
     {
         return $this->hasMany(Task::class);
     }
+
+    public function addTask($description)
+    {
+        $this->tasks()->create(compact('description'));
+    }
 }
