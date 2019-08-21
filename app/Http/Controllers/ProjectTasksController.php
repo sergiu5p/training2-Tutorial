3d1d95ec557ce323af4ce779d79425386d7e198c
sergiu5p <sergiu.purcariu@5psolutions.eu>
2019-08-21 11:46:20 +0300

    Create store method

--- app/Http/Controllers/ProjectTasksController.php
+++ app/Http/Controllers/ProjectTasksController.php
@@ -2,17 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Project;
 use App\Task;
 use Illuminate\Http\Request;
 
 class ProjectTasksController extends Controller
 {
+    public function store(Project $project)
+    {
+        Task::create([
+            'project_id' => $project->id,
+            'description' => request('description')
+        ]);
+
+        return back();
+    }
+
     public function update(Task $task)
     {
         $task->update([
             'completed' => request()->has('completed')
         ]);
+
         return back();
     }
-
 }
