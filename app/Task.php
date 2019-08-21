a22042064b962b88605501d1771eee333c7d4649
sergiu5p <sergiu.purcariu@5psolutions.eu>
2019-08-21 12:41:48 +0300

    Create complete method

--- app/Task.php
+++ app/Task.php
@@ -8,6 +8,11 @@ class Task extends Model
 {
     protected $guarded = [];
 
+    public function complete($completed = true)
+    {
+        $this->update(compact('completed'));
+    }
+
     public function project()
     {
         return $this->belongsTo(Project::class);
