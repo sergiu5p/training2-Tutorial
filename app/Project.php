ed319a95a33d5923a59ed36a07e6c5ecdac8c397
sergiu5p <sergiu.purcariu@5psolutions.eu>
2019-08-23 14:49:28 +0300

    Create functions owner and boot

--- app/Project.php
+++ app/Project.php
@@ -2,11 +2,29 @@
 
 namespace App;
 
+use App\Mail\ProjectCreated;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Mail;
 
 class Project extends Model
 {
     protected $fillable = ['owner_id', 'title', 'description'];
+
+    public static function boot()
+    {
+        parent::boot();
+
+        static::created(function ($project) {
+            Mail::to($project->owner->email)->send(
+                new ProjectCreated($project)
+            );
+        });
+    }
+
+    public function owner()
+    {
+        return $this->belongsTo(User::class);
+    }
 
     public function tasks()
     {
