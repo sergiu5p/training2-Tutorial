5a5eb2a640adb941f366b0d1fdd7d1c2b9288f49
sergiu5p <sergiu.purcariu@5psolutions.eu>
2019-08-23 15:40:06 +0300

    Remove unused imports.

--- app/Listeners/SendProjectCreatedNotification.php
+++ app/Listeners/SendProjectCreatedNotification.php
@@ -4,8 +4,6 @@ namespace App\Listeners;
 
 use App\Mail\ProjectCreated as ProjectCreatedMail;
 use App\Events\ProjectCreated;
-use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Support\Facades\Mail;
 
 class SendProjectCreatedNotification
