ea5d747e4c1d92584e095b83eda8b6398afc1f0c
sergiu5p <sergiu.purcariu@5psolutions.eu>
2019-08-23 15:11:42 +0300

    Send email o the user in the event

--- app/Listeners/SendProjectCreatedNotification.php
+++ app/Listeners/SendProjectCreatedNotification.php
@@ -2,8 +2,11 @@
 
 namespace App\Listeners;
 
+use App\Mail\ProjectCreated as ProjectCreatedMail;
+use App\Events\ProjectCreated;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Support\Facades\Mail;
 
 class SendProjectCreatedNotification
 {
@@ -20,11 +23,13 @@ class SendProjectCreatedNotification
     /**
      * Handle the event.
      *
-     * @param  object  $event
+     * @param  ProjectCreated  $event
      * @return void
      */
-    public function handle($event)
+    public function handle(ProjectCreated $event)
     {
-        //
+        Mail::to($event->project->owner->email)->send(
+            new ProjectCreatedMail($event->project)
+        );
     }
 }
