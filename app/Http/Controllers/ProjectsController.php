14acb0ffeccddb3c2dcb9c8c3438c58877a2c463
sergiu5p <sergiu.purcariu@5psolutions.eu>
2019-08-23 15:01:58 +0300

    Add event()

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\ProjectCreated;
 use App\Project;
 use Illuminate\Http\Request;
 
@@ -55,7 +56,9 @@ class ProjectsController extends Controller
 
         $attributes['owner_id'] = auth()->id();
 
-        Project::create($attributes);
+        $project = Project::create($attributes);
+
+        event(new ProjectCreated($project));
 
         return redirect('/projects');
     }
